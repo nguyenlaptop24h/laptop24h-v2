585df3a3e3d2123b77629b3470f9cf146fbdcf50
nguyenlaptop24h <nguyenlaptop24h@gmail.com>
2026-04-17 03:24:53 +0700

    chore: bump inventory.js to v11

--- core/router.js
+++ core/router.js
@@ -38,7 +38,7 @@ export function initRouter() {
   Promise.all([
     import('../modules/repairs.js'),
     import('../modules/sales.js'),
-    import('../modules/inventory.js?v=10'),
+    import('../modules/inventory.js?v=11'),
     import('../modules/customers.js'),
     import('../modules/debts.js'),
     import('../modules/stats.js'),
