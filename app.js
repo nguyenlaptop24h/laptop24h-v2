d98102a31037aed7fc3338c0cd828ab80334f920
nguyenlaptop24h <nguyenlaptop24h@gmail.com>
2026-04-17 03:55:28 +0700

    chore: cache-bust router.js import v2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // app.js - Bootstrap entry point
 import { initDB } from './core/db.js';
 import { initAuth } from './core/auth.js';
-import { initRouter } from './core/router.js';
+import { initRouter } from './core/router.js?v=2';
 import { initUI } from './core/ui.js';
 
 async function main() {
