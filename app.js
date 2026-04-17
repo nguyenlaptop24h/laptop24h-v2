01a36154b4e4b290a49bd26efa03e7267c677e65
nguyenlaptop24h <nguyenlaptop24h@gmail.com>
2026-04-17 12:31:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // app.js - Bootstrap entry point
 import { initDB } from './core/db.js';
 import { initAuth } from './core/auth.js';
-import { initRouter } from './core/router.js?v=16';
+import { initRouter } from './core/router.js?v=17';
 import { initUI } from './core/ui.js';
 
 async function main() {
